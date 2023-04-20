a177fcaaeb2e9caf11a64dd192c3df7553143bb6
Erick <erick.nahuelpaiva.001@gmail.com>
2023-04-20 12:37:27 -0300

    ejemplo termiando

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function pedirInformacion(tipoInfo){
+    return prompt("Necesito " + tipoInfo +" informacion");
+}
+
+
+let infoRecibida = pedirInformacion("x");
+let infoRecibida2 = pedirInformacion("y");
+let infoRecibida3 = pedirInformacion("w");
+let infoRecibida4 = pedirInformacion("z");
+
+
+
+
+/* //definir una funcion que sume 2 numeros recibidos y devuelva el resultado
+function suma(numeroUno, numeroDos){
+    return (numeroUno + numeroDos);
+}
+
+function division(numeroA, numeroB){
+    return (numeroA / numeroB)
+}
+
+function mostrarMsj(nombre){
+    console.log("Hola "+ nombre + " bienvenido!");
+}
+
+//llamada a la funcion
+console.log(  suma(-4,-2)  );
+console.log(  division(4, 2)  );
+
+mostrarMsj("alguien");
+mostrarMsj("usuario 1"); */
